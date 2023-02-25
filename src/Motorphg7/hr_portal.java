82bcdf6cb542d948287f62436614629048c25cae
lr.lguirre <lr.lguirre@mmdc.mcl.edu.ph>
2023-02-25 22:40:14 +0800

    Added menu 4, totalhours_calculator

--- src/Motorphg7/hr_portal.java
+++ src/Motorphg7/hr_portal.java
@@ -12,13 +12,15 @@ public class hr_portal {
         System.out.println("[1] Employee Search");
         System.out.println("[2] Work Hours Calculator");
         System.out.println("[3] Work Week Calculator");
-        System.out.println("[4] Exit");
+        System.out.println("[4] Work Week Calculator");
+        System.out.println("[5] Exit");
         String hr_function = sc.nextLine();
         switch (hr_function) {
             case "1" -> employee_search.entry();
             case "2" -> workhours_calculator.entry();
             case "3" -> workweek_calculator.entry();
-            case "4" -> exit.Exit();
+            case "4" -> totalhours_calculator.entry();
+            case "5" -> exit.Exit();
             default -> {
                 System.out.println("Please select a correct HR function.");
                 hrPortal();
